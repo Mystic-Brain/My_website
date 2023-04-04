6f8a03fa50fe67bffee7df29b031b7c80b825335
Sanket Sawale <sawalesanket@gmail.com>
2023-04-05 01:37:41 +0530

    footer_update

--- webapp/src/components/Footer/footerr.jsx
+++ webapp/src/components/Footer/footerr.jsx
@@ -1,69 +1,83 @@
-import React from 'react'
-import './footerr.css'
-import Github from '../../img/github_ss.png'
-import LinkedIn from '../../img/linkedin_sss.png'
-import Instagram from '../../img/instagram_sss.png'
-import Facebook from '../../img/facebook_sss.png'
-import Twitter from '../../img/twitter_sss.png'
-import Location from '../../img/location.png'
-import Calling from '../../img/calling.png'
-import Gmail from '../../img/gmail.png'
-
-
-
+import React from "react";
+import "./footerr.css";
+import Github from "../../img/github_ss.png";
+import LinkedIn from "../../img/linkedin_sss.png";
+import Instagram from "../../img/instagram_sss.png";
+import Facebook from "../../img/facebook_sss.png";
+import Twitter from "../../img/twitter_sss.png";
+import Location from "../../img/location.png";
+import Calling from "../../img/calling.png";
+import Gmail from "../../img/gmail.png";
 
 function Footer() {
   return (
-
-    <div className='footer'>
-        <div className="left">        
-            <div className='icon'> 
-              <img src={Location} alt =''/>
-              <span>Viman Nagar, Pune</span>
-            </div>
-         
-        
-            <div className='icon'> 
-              <img src={Calling } alt =''/>
-              <span>+918087138601</span>
-            </div>
-
-          
-            <div className='icon'> 
-              <img src={Gmail} alt =''/>
-              <span>sawalesanket@gmail.com</span>
-            </div>
+    <div className="footer">
+      <div className="left">
+        <div className="icon">
+          <img src={Location} alt="" />
+          <span>Viman Nagar, Pune</span>
         </div>
 
-        <div className="right">
-            <div className="i-icon">
-              <a href='https://github.com/Mystic-Brain'>
-              <img src={Github} alt =''/>
-              </a>
-              <a href='https://www.linkedin.com/in/sanket-sawale-111571a3/'>
-              <img src={LinkedIn} alt =''/>
-              </a>
-              <a href='https://www.instagram.com/sank8_patil/'>
-              <img src={Instagram} alt =''/>
-              </a>
-              <a href='https://www.facebook.com/CoOlsank8'>
-              <img src={Facebook} alt =''/>
-              </a>
-              <a href='https://twitter.com/cooksank8'>
-              <img src={Twitter} alt =''/>
-              </a>
-            </div>
+        <div className="icon">
+          <img src={Calling} alt="" />
+          <span>+918087138601</span>
         </div>
 
-        <div className="blur111" style={{background:"rgb(238, 210, 255)"}}></div>
-        <div className="blur111" style={{background:"#C1F5FF",
-                                        top:'40%',width:'40%',height:'11rem',left:'-9rem'}}></div>
+        <div className="icon">
+          <img src={Gmail} alt="" />
+          <span>sawalesanket@gmail.com</span>
+        </div>
+      </div>
 
+      <div className="right">
 
         
-  </div>
-    
-  )
+        <div className="i-icon">
+          <a href="https://github.com/Mystic-Brain">
+            <img src={Github} alt="" />
+          </a>
+          <a href="https://www.linkedin.com/in/sanket-sawale-111571a3/">
+            <img src={LinkedIn} alt="" />
+          </a>
+          <a href="https://www.instagram.com/sank8_patil/">
+            <img src={Instagram} alt="" />
+          </a>
+          <a href="https://www.facebook.com/CoOlsank8">
+            <img src={Facebook} alt="" />
+          </a>
+          <a href="https://twitter.com/cooksank8">
+            <img src={Twitter} alt="" />
+          </a>
+        </div>
+        <div className="txt">Looking for a challenging role in a reputable organization to utilize
+          my technical, database, and management skills for the growth of the
+          organization as well as to enhance my knowledge about new and emerging
+          trends the IT sector.
+        <hr ></hr>
+        <div className="txt2">
+        Copyright © 2023 | Made by Sanket
+        </div>
+
+        </div>
+      </div>
+
+      <div
+        className="blur111"
+        style={{ background: "rgb(238, 210, 255)" }}
+      ></div>
+      <div
+        className="blur111"
+        style={{
+          background: "#C1F5FF",
+          top: "40%",
+          width: "40%",
+          height: "11rem",
+          left: "-9rem",
+        }}
+      ></div>
+      
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
