5789844834f43b05dcedc0bb645d7b820dd67942
sanket <sawalesanket@gmail.com>
2024-01-08 11:12:15 +0530

    changesP1

--- webapp/src/components/Navbar/Navbar.jsx
+++ webapp/src/components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const navbar = () => {
     <div className="n-wrapper" id="Navbar">
       {/* left */}
       <div className="n-left">
-        <div className="n-name">Sank8</div>
+        <div className="n-name">Sanket</div>
         <Toggle/>
        
       </div>
